72e5f5f6615f09da94cfcc226f616486bca37a05
Bin Wang <wbin00@gmail.com>
2015-01-06 15:59:45 +0800

    init nn and backprop algorithm

---
+++ backprop.m
@@ -0,0 +1,30 @@
+
+function [next_nn, cost] = backprop(nn, rate, x, y)
+	l = size(nn.activations)(2);
+
+	% forward
+	nn.activations{1} = x;
+	for i = 2:l
+		nn.activations{i} = nn.activations{i-1} * nn.weights{i-1} .+ nn.bias{i-1};
+		nn.activations{i} = nn.sigmod(nn.activations{i});
+	end
+
+	% backprop
+	cost{l} = cost_func(y, nn.activations{l});
+	for i = 1:(l-1)
+		j = l - i;
+
+		cost{j} = cost{j+1} * transpose(nn.weights{j});
+		d_bias = nn.sigmod_prime(cost{j + 1});
+		d_weight = transpose(nn.activations{j}) * nn.sigmod_prime(cost{j + 1});
+
+		nn.weights{j} = nn.weights{j} - (d_weight .* rate);
+		nn.bias{j} = nn.bias{j} - (d_bias .* rate);
+	end
+
+	next_nn = nn;
+end
+
+function r = cost_func(y, a)
+	r = (y - a) .^ 2;
+end
---
+++ init_nn.m
@@ -0,0 +1,23 @@
+
+function nn = init_nn(layers)
+	l = size(layers)(2);
+
+	for i = 1:(l-1)
+		nn.activations{i} = zeros(1, layers(i));
+		nn.weights{i} = randn(layers(i), layers(i+1));
+		nn.bias{i} = randn(1, layers(i+1));
+	end
+
+	nn.activations{l} = zeros(1, layers(l));
+
+	nn.sigmod = @sigmod;
+	nn.sigmod_prime = @sigmod_prime;
+end
+
+function output = sigmod(input)
+	output = 1 ./ (1 .+ e .^ input);
+end
+
+function output = sigmod_prime(input)
+	output = sigmod(input) .* (1 .- sigmod(input));
+end
